743f19072ad3225e1029ba1ce7aef3e8821c9a8f
Leandro Lopes <leandro_carlos@uol.com.br>
2021-11-05 11:04:59 -0300

    referencias e final do desafio para entrega

---
+++ ebytr/frontend/src/referencias.js
@@ -0,0 +1,17 @@
+/* 
+Minhas referencias para desenvover o desafio.
+
+https://trybecourse.slack.com/archives/C01PLFW7347/p1633459424040200
+https://app.betrybe.com/course/live-lectures/sd-cohort-11#aula-262-arquitetura-de-software-camada-de-controller-e-service
+https://app.betrybe.com/course/live-lectures/sd-cohort-11#aula-264-arquitetura-de-software-testando-as-camadas
+https://app.betrybe.com/course/back-end/nodejs-camada-de-servico-e-arquitetura-rest-e-restful/
+arquitetura-de-software-testando-as-camadas/33e05d58-0be4-48c0-adb9-f2f3d647c96e/o-que-vamos-aprender/e25b0535-af02-4566-adc1-d135c77f6e8f?use_case=calendar
+https://app.betrybe.com/course/back-end/nodejs-camada-de-servico-e-arquitetura-rest-e-restful/
+arquitetura-de-software-camada-de-model/69147096-f19d-4ab4-a839-906359d79172/o-que-vamos-aprender/b4ba70f3-dc4b-41e6-bf1c-806530cb59de?use_case=calendar
+https://www.youtube.com/watch?v=RMIV-R_zFnY&t=0s
+https://www.youtube.com/watch?v=Hab5lypI_z4
+https://www.mongodb.com/mern-stack
+https://pt-br.reactjs.org/
+https://stackoverflow.com/questions/19697858/referenceerror-textencoder-is-not-defined
+https://www.npmjs.com/package/cors
+ */
